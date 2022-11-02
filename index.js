dd24f5650fa72dea999f4ff5b17dde6faecd3794
FennectDev <fennectdev@gmail.com>
2022-11-01 19:36:16 -0500

    Chat design improved

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-let message;
+let message
+let messageArray = new Array()
+
 addEventListener('keypress', event => sendMessage(event))
 
 function sendMessage(event) {
     var key = event.key
 
-    if (key === "Enter" && event.shiftKey === false) {
+    let message = document.querySelector("textarea").value
+
+    if (key === "Enter" && event.shiftKey === false && message.length > 0) {
       window.event.preventDefault()
-      message = document.querySelector("textarea").value
       document.querySelector("textarea").value = ""
-      document.querySelector("body").innerHTML += message;
+
+      if (messageArray.length >= 5) {
+        messageArray[0].remove()
+        messageArray.splice(messageArray[0], 1)
+      }
+
+      let messageContainer = displayMessage(message)
+      messageArray.push(messageContainer)
+    } else if (key === "Enter" && event.shiftKey === false && message.length === 0) {
+      window.event.preventDefault()
     }
     return
+}
+
+function displayMessage(message) {
+  let messageContainer = document.createElement("div")
+  messageContainer.className = "messagecontainer"
+  document.querySelector(".chat").append(messageContainer)
+  messageContainer.innerHTML += message;
+  return messageContainer
 }
